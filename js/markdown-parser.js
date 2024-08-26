09f44a288ebf9f6e852a25ee4c230fcd11164c48
MN Industries <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-08-26 17:18:41 -0500

    Create markdown-parser.js

---
+++ js/markdown-parser.js
@@ -0,0 +1,36 @@
+function markdownToHtml(markdown) {
+    let html = markdown;
+
+    // Headers
+    html = html.replace(/^###### (.*$)/gm, '<h6>$1</h6>');
+    html = html.replace(/^##### (.*$)/gm, '<h5>$1</h5>');
+    html = html.replace(/^#### (.*$)/gm, '<h4>$1</h4>');
+    html = html.replace(/^### (.*$)/gm, '<h3>$1</h3>');
+    html = html.replace(/^## (.*$)/gm, '<h2>$1</h2>');
+    html = html.replace(/^# (.*$)/gm, '<h1>$1</h1>');
+
+    // Lists
+    html = html.replace(/^\* (.*$)/gm, '<ul><li>$1</li></ul>');
+    html = html.replace(/^\+ (.*$)/gm, '<ul><li>$1</li></ul>');
+    html = html.replace(/^\- (.*$)/gm, '<ul><li>$1</li></ul>');
+
+    // Links
+    html = html.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>');
+
+    // Inline Code
+    html = html.replace(/`([^`]+)`/g, '<code>$1</code>');
+
+    // Code Blocks
+    html = html.replace(/```(.*?)```/gs, '<pre><code>$1</code></pre>');
+
+    // Bold
+    html = html.replace(/\*\*([^\*]+)\*\*/g, '<strong>$1</strong>');
+
+    // Italics
+    html = html.replace(/\*([^\*]+)\*/g, '<em>$1</em>');
+
+    // Line Breaks
+    html = html.replace(/\n/g, '<br>');
+
+    return html;
+}
