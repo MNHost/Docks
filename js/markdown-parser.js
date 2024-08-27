d0e827b4fd7e9f378dc0f3183673d1d2a87d8598
MN Industries <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-08-27 11:10:09 -0500

    Update markdown-parser.js

--- js/markdown-parser.js
+++ js/markdown-parser.js
@@ -16,12 +16,15 @@ function markdownToHtml(markdown) {
     
     // Buttons
     html = html.replace(/\[button:([^\]]+)\]\(([^)]+)\)/g, '<button class="button" onclick="window.location.href=\'$2\'">$1</button>');
-        html = html.replace(/!\[([^\]]*)\]\(([^)]+)\)\s*\{([^}]*)\}/g, function (match, alt, src, size) {
+       // Images with size and alignment control
+    html = html.replace(/!\[([^\]]*)\]\(([^)]+)\)\s*\{([^}]*)\}/g, function (match, alt, src, properties) {
         let width = '';
         let height = '';
+        let align = '';
 
-        const widthMatch = size.match(/width=(\d+)/);
-        const heightMatch = size.match(/height=(\d+)/);
+        const widthMatch = properties.match(/width=(\d+)/);
+        const heightMatch = properties.match(/height=(\d+)/);
+        const alignMatch = properties.match(/align=(left|center|right)/);
 
         if (widthMatch) {
             width = `width="${widthMatch[1]}" `;
@@ -29,10 +32,20 @@ function markdownToHtml(markdown) {
         if (heightMatch) {
             height = `height="${heightMatch[1]}" `;
         }
+        if (alignMatch) {
+            align = alignMatch[1]; // Get the alignment value
+        }
 
-        return `<img src="${src}" alt="${alt}" ${width}${height}>`;
+        // Create the image HTML with alignment
+        let imgHtml = `<img src="${src}" alt="${alt}" ${width}${height}>`;
+
+        // Wrap the image in a div with an alignment class
+        if (align) {
+            imgHtml = `<div class="img-${align}">${imgHtml}</div>`;
+        }
+
+        return imgHtml;
     });
-    html = html.replace(/!\[([^\]]*)\]\(([^)]+)\)/g, '<img src="$2" alt="$1">');
 
     // Links
     html = html.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>');
