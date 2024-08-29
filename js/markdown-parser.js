676331eba6352a20ecada93be9c6018b0cdc6cc1
MN Industries <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-08-29 14:01:25 -0500

    Update markdown-parser.js

--- js/markdown-parser.js
+++ js/markdown-parser.js
@@ -1,6 +1,26 @@
 function markdownToHtml(markdown) {
     let html = markdown;
+ // Tabs
+    html = html.replace(/::: tabs\s*([\s\S]*?):::$/gm, function (match, content) {
+        // Extract tab titles and contents
+        const tabs = content.split(/(?=###\s)/).map(tab => tab.trim());
 
+        // Generate HTML for tabs
+        const tabButtons = tabs.map((tab, index) => {
+            const title = tab.split('\n')[0].replace(/^###\s/, '');
+            return `<button class="tab-button" data-tab="tab${index}">${title}</button>`;
+        }).join('');
+
+        const tabContents = tabs.map((tab, index) => {
+            const content = tab.replace(/^###\s.*\n/, ''); // Remove title line
+            return `<div class="tab-content" data-tab="tab${index}">${markdownToHtml(content)}</div>`;
+        }).join('');
+
+        return `<div class="tabs-container">
+                    <div class="tab-buttons">${tabButtons}</div>
+                    <div class="tab-contents">${tabContents}</div>
+                </div>`;
+    });
     // Headers
     html = html.replace(/^###### (.*)$/gm, '<h6>$1</h6>');
     html = html.replace(/^##### (.*)$/gm, '<h5>$1</h5>');
@@ -16,12 +36,41 @@ function markdownToHtml(markdown) {
     
     // Buttons
     html = html.replace(/\[button:([^\]]+)\]\(([^)]+)\)/g, '<button class="button" onclick="window.location.href=\'$2\'">$1</button>');
+       // Images with size and alignment control
+    html = html.replace(/!\[([^\]]*)\]\(([^)]+)\)\s*\{([^}]*)\}/g, function (match, alt, src, properties) {
+        let width = '';
+        let height = '';
+        let align = '';
 
+        const widthMatch = properties.match(/width=(\d+)/);
+        const heightMatch = properties.match(/height=(\d+)/);
+        const alignMatch = properties.match(/align=(left|center|right)/);
+
+        if (widthMatch) {
+            width = `width="${widthMatch[1]}" `;
+        }
+        if (heightMatch) {
+            height = `height="${heightMatch[1]}" `;
+        }
+        if (alignMatch) {
+            align = alignMatch[1]; // Get the alignment value
+        }
+
+        // Create the image HTML with alignment
+        let imgHtml = `<img src="${src}" alt="${alt}" ${width}${height}>`;
+
+        // Wrap the image in a div with an alignment class
+        if (align) {
+            imgHtml = `<div class="img-${align}">${imgHtml}</div>`;
+        }
+
+        return imgHtml;
+    });
+html = html.replace(/!\[([^\]]*)\]\(([^)]+)\)/g, '<img src="$2" alt="$1">');
     // Links
     html = html.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>');
 
     // Images
-    html = html.replace(/!\[([^\]]*)\]\(([^)]+)\)/g, '<img src="$2" alt="$1">');
 
     // Code Blocks
     html = html.replace(/```(\w*)\n([\s\S]*?)\n```/g, function (match, p1, p2) {
