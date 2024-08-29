c90a502f2fa9f76f342a790725c3a00c23afea2d
MN Industries <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-08-29 13:45:36 -0500

    Update markdown-parser.js

--- js/markdown-parser.js
+++ js/markdown-parser.js
@@ -1,10 +1,11 @@
 function markdownToHtml(markdown) {
     let html = markdown;
-
-    // Tabs
+ // Tabs
     html = html.replace(/::: tabs\s*([\s\S]*?):::$/gm, function (match, content) {
+        // Extract tab titles and contents
         const tabs = content.split(/(?=###\s)/).map(tab => tab.trim());
 
+        // Generate HTML for tabs
         const tabButtons = tabs.map((tab, index) => {
             const title = tab.split('\n')[0].replace(/^###\s/, '');
             return `<button class="tab-button" data-tab="tab${index}">${title}</button>`;
@@ -20,7 +21,6 @@ function markdownToHtml(markdown) {
                     <div class="tab-contents">${tabContents}</div>
                 </div>`;
     });
-
     // Headers
     html = html.replace(/^###### (.*)$/gm, '<h6>$1</h6>');
     html = html.replace(/^##### (.*)$/gm, '<h5>$1</h5>');
@@ -33,11 +33,10 @@ function markdownToHtml(markdown) {
     html = html.replace(/^\* (.*)$/gm, '<ul><li>$1</li></ul>');
     html = html.replace(/^\+ (.*)$/gm, '<ul><li>$1</li></ul>');
     html = html.replace(/^\- (.*)$/gm, '<ul><li>$1</li></ul>');
-
+    
     // Buttons
     html = html.replace(/\[button:([^\]]+)\]\(([^)]+)\)/g, '<button class="button" onclick="window.location.href=\'$2\'">$1</button>');
-
-    // Images with size and alignment control
+       // Images with size and alignment control
     html = html.replace(/!\[([^\]]*)\]\(([^)]+)\)\s*\{([^}]*)\}/g, function (match, alt, src, properties) {
         let width = '';
         let height = '';
@@ -57,25 +56,26 @@ function markdownToHtml(markdown) {
             align = alignMatch[1]; // Get the alignment value
         }
 
+        // Create the image HTML with alignment
         let imgHtml = `<img src="${src}" alt="${alt}" ${width}${height}>`;
 
+        // Wrap the image in a div with an alignment class
         if (align) {
             imgHtml = `<div class="img-${align}">${imgHtml}</div>`;
         }
 
         return imgHtml;
     });
-
-    // Images (basic)
-    html = html.replace(/!\[([^\]]*)\]\(([^)]+)\)/g, '<img src="$2" alt="$1">');
-
+html = html.replace(/!\[([^\]]*)\]\(([^)]+)\)/g, '<img src="$2" alt="$1">');
     // Links
     html = html.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>');
 
-    // Code Blocks with Syntax Highlighting
-    html = html.replace(/```(\w*)\n([\s\S]*?)\n```/g, function (match, lang, code) {
-        const highlightedCode = `<pre><code class="language-${lang}">${hljs.highlight(lang, code).value}</code></pre>`;
-        return highlightedCode;
+    // Images
+
+    // Code Blocks
+    html = html.replace(/```(\w*)\n([\s\S]*?)\n```/g, function (match, p1, p2) {
+        // p1 is the language specifier, p2 is the code
+        return '<pre class="code-block"><code class="' + p1 + '">' + p2 + '</code></pre>';
     });
 
     // Inline Code
