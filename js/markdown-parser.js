b00ba1854589dcc205f5480e7dcab9272ceee0ac
MN Industries <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-08-29 13:59:42 -0500

    Update markdown-parser.js

--- js/markdown-parser.js
+++ js/markdown-parser.js
@@ -16,41 +16,12 @@ function markdownToHtml(markdown) {
     
     // Buttons
     html = html.replace(/\[button:([^\]]+)\]\(([^)]+)\)/g, '<button class="button" onclick="window.location.href=\'$2\'">$1</button>');
-       // Images with size and alignment control
-    html = html.replace(/!\[([^\]]*)\]\(([^)]+)\)\s*\{([^}]*)\}/g, function (match, alt, src, properties) {
-        let width = '';
-        let height = '';
-        let align = '';
-
-        const widthMatch = properties.match(/width=(\d+)/);
-        const heightMatch = properties.match(/height=(\d+)/);
-        const alignMatch = properties.match(/align=(left|center|right)/);
-
-        if (widthMatch) {
-            width = `width="${widthMatch[1]}" `;
-        }
-        if (heightMatch) {
-            height = `height="${heightMatch[1]}" `;
-        }
-        if (alignMatch) {
-            align = alignMatch[1]; // Get the alignment value
-        }
-
-        // Create the image HTML with alignment
-        let imgHtml = `<img src="${src}" alt="${alt}" ${width}${height}>`;
-
-        // Wrap the image in a div with an alignment class
-        if (align) {
-            imgHtml = `<div class="img-${align}">${imgHtml}</div>`;
-        }
-
-        return imgHtml;
-    });
 
     // Links
     html = html.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>');
 
     // Images
+    html = html.replace(/!\[([^\]]*)\]\(([^)]+)\)/g, '<img src="$2" alt="$1">');
 
     // Code Blocks
     html = html.replace(/```(\w*)\n([\s\S]*?)\n```/g, function (match, p1, p2) {
